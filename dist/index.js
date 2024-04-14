6ec277d1b738418ae0ab89ec6a8fc9046fb17ac7
arashsamandar <arash.internet@gmail.com>
2024-04-15 03:23:32 +0330

    ready to get express version

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,9 @@
-"use strict";
-// check to see vercel functionality
-// lets check whether changes persist or not
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.listen(3000, () => console.log('hello arash server is up'));
+const express = require('express');
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello arash and welcome to your new web application');
+    const expressVersion = express.version;
+    res.send(`hello arash from Express Version : ${expressVersion}`);
 });
 
 app.get('/aarash',(req,res)=>{
