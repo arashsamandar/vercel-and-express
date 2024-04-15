73ccbfe4d5e9eb55808c4b937fe00ba8e41d2a50
arashsamandar <arash.internet@gmail.com>
2024-04-15 13:22:00 +0330

    checking again aarash

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express();
+app.listen(3000,()=>console.log('application ready to use'));
 app.get('/',(req,res)=>{
 	res.send("hello aarash samandar from Ts");
 });
