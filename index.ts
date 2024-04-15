212fb92f65f8ca07ab0f7493063258331dd0ed60
arashsamandar <arash.internet@gmail.com>
2024-04-15 03:39:14 +0330

    fixing ts files too

--- index.ts
+++ index.ts
@@ -2,6 +2,5 @@ import express from 'express';
 const app = express();
 app.listen(3000,()=>console.log('hello arash server is up'));
 app.get('/',(req,res)=>{
-	const expressVersiono = express.version;
-	res.send(`hello arash from : ${expressVersiono}`);
+	res.send("hello aarash samandar from Ts");
 });
