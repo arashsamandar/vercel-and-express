8d73d772a522324961dccec8ba0a5bdb1661eb0b
arashsamandar <arash.internet@gmail.com>
2024-04-15 12:52:32 +0330

    new address added to vercel arash

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.listen(3000, () => console.log('hello arash server is up'));
 app.get('/', (req, res) => {
     res.send("hello aarash samandar from Ts");
 });
 app.get('/aarash', (req, res) => {
     res.send('hello from aarash route babe');
+});
+app.get('/newaddress', (req, res) => {
+    res.send('new address added to the Vercel arash sir');
 });
--- index.ts
+++ index.ts
@@ -2,12 +2,14 @@ import express from 'express';
 import mongodb from 'mongodb';
 
 const app = express();
-app.listen(3000,()=>console.log('hello arash server is up'));
-
 app.get('/',(req,res)=>{
 	res.send("hello aarash samandar from Ts");
 });
 
 app.get('/aarash',(req,res)=>{
 	res.send('hello from aarash route babe');
-})
+});
+
+app.get('/newaddress',(req,res)=>{
+	res.send('new address added to the Vercel arash sir');
+});
