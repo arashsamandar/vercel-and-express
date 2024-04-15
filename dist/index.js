906a9442848e5bc080c5ec47a623c7f2031fbd58
arashsamandar <arash.internet@gmail.com>
2024-04-15 03:43:58 +0330

    adding new routes babyyyyy

--- dist/index.js
+++ dist/index.js
@@ -9,3 +9,6 @@ app.listen(3000, () => console.log('hello arash server is up'));
 app.get('/', (req, res) => {
     res.send("hello aarash samandar from Ts");
 });
+app.get('/aarash', (req, res) => {
+    res.send('hello from aarash route babe');
+});
--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
 const app = express();
 app.listen(3000,()=>console.log('hello arash server is up'));
+
 app.get('/',(req,res)=>{
 	res.send("hello aarash samandar from Ts");
 });
+
+app.get('/aarash',(req,res)=>{
+	res.send('hello from aarash route babe');
+})
