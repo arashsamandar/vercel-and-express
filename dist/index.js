f9f2719c72ffd8ea038cd14d0ebe76cf4634b058
arashsamandar <arash.internet@gmail.com>
2024-04-15 13:22:36 +0330

    all set

--- dist/index.js
+++ dist/index.js
@@ -5,6 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+app.listen(3000, () => console.log('application ready to use'));
 app.get('/', (req, res) => {
     res.send("hello aarash samandar from Ts");
 });
