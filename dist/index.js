5dcda41761f814057a302d069686ceb1faba90b7
arashsamandar <arash.internet@gmail.com>
2024-04-15 03:39:48 +0330

    fixing dist too

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const app = express();
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.listen(3000, () => console.log('hello arash server is up'));
 app.get('/', (req, res) => {
-    res.send("hello baby baby boy aarash :)");
-});
-
-app.get('/aarash',(req,res)=>{
-	res.send('welcome to route aarash');
+    res.send("hello aarash samandar from Ts");
 });
