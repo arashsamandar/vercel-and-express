c1b105f6ec4bb38bfd5a3f0a6000e418d305dedd
arashsamandar <arash.internet@gmail.com>
2024-07-01 13:07:02 +0330

    soms soss

--- index.ts
+++ index.ts
@@ -5,9 +5,5 @@ app.use(express.static('public'));
 app.listen(3000,()=>console.log('application ready to use'));
 
 app.get('/',(req,res)=>{
-	res.sendFile('index.html',{root:__dirname + '\\public'});
-});
-
-app.get('/arash',(req,res)=>{
-	res.send('hello arash from my new add in Vercel');
+	res.sendFile('index.html',{root:__dirname + '\\public'})
 });
