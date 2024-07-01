b88058567316fc912bb1dce8bc0ea53fd60a7cc2
arashsamandar <arash.internet@gmail.com>
2024-07-01 12:23:00 +0330

    new in july 2024 from arash samandar

--- index.ts
+++ index.ts
@@ -7,3 +7,7 @@ app.listen(3000,()=>console.log('application ready to use'));
 app.get('/',(req,res)=>{
 	res.sendFile('index.html',{root:__dirname + '\\public'})
 });
+
+app.get('/arash',(req,res)=>{
+	res.send('hello arash from my new add in Vercel');
+});
