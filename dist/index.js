20960c41303709f79d989b18e6294763e6900d35
arashsamandar <arash.internet@gmail.com>
2024-07-01 13:29:39 +0330

    saw arash works

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,10 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const app = (0, express_1.default)();
-app.use(express_1.default.static('public'));
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
 app.listen(3000, () => console.log('application ready to use'));
+
 app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname + '\\public' });
+    res.sendFile('/index.html', { root: __dirname });
 });
+
