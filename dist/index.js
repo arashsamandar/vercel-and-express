46aff26d61c388ed788a0a34992b2a390202cf1d
arashsamandar <arash.internet@gmail.com>
2024-07-02 20:02:17 +0330

    new new new

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-// app.use(express.static('public'));
 app.listen(3000, () => console.log('application ready to use'));
-// app.get('/',(req,res)=>{
-// 	res.sendFile('index.html',{root:__dirname + '\\public'})
-// });
 app.get('/api/helloarash', (req, res) => {
     res.send('hello arash this one works ?? really ??');
 });
--- index.ts
+++ index.ts
@@ -1,12 +1,7 @@
 import express from 'express';
 
 const app = express();
-// app.use(express.static('public'));
 app.listen(3000,()=>console.log('application ready to use'));
-
-// app.get('/',(req,res)=>{
-// 	res.sendFile('index.html',{root:__dirname + '\\public'})
-// });
 
 app.get('/api/helloarash',(req,res)=>{
 	res.send('hello arash this one works ?? really ??');
