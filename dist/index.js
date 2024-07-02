5125e8b87faccdd360d21dbb0a4f7c5c63d071b1
arashsamandar <arash.internet@gmail.com>
2024-07-02 19:27:10 +0330

    old public route deleted and new helloarash route was added

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+// app.use(express.static('public'));
 app.listen(3000, () => console.log('application ready to use'));
-
-app.get('/', (req, res) => {
-    res.sendFile('./public/index.html');
-});
-
-app.get('/samandar', (req, res) => {
-    res.send('some bullshits here');
+// app.get('/',(req,res)=>{
+// 	res.sendFile('index.html',{root:__dirname + '\\public'})
+// });
+app.get('/helloarash', (req, res) => {
+    res.send('hello arash this one works ?? really ??');
 });
--- index.ts
+++ index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 
 const app = express();
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.listen(3000,()=>console.log('application ready to use'));
 
-app.get('/',(req,res)=>{
-	res.sendFile('index.html',{root:__dirname + '\\public'})
-});
+// app.get('/',(req,res)=>{
+// 	res.sendFile('index.html',{root:__dirname + '\\public'})
+// });
+
+app.get('/helloarash',(req,res)=>{
+	res.send('hello arash this one works ?? really ??');
+})
