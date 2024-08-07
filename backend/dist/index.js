8ce038468b44347b73213602901711201167a80d
arashsamandar <arash.internet@gmail.com>
2024-08-07 22:55:36 +0330

    tailwind was removed from "BackEnd"

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,11 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const startTime = Date.now();
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.listen(3000, () => console.log('application ready to use'));
 app.get('/api/helloarash', (req, res) => {
-    res.send('hello arash this one works ?? really ??');
+    res.send(`took ${Date.now() - startTime}ms to respond`);
 });
 app.get('/api/helloarash2', (req, res) => {
     res.send('hello arash this one works On Two 2');
--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,11 @@
+const startTime = Date.now();
 import express from 'express';
 
 const app = express();
 app.listen(3000,()=>console.log('application ready to use'));
 
 app.get('/api/helloarash',(req,res)=>{
-	res.send('hello arash this one works ?? really ??');
+	res.send(`took ${Date.now() - startTime}ms to respond`);
 });
 
 app.get('/api/helloarash2',(req,res)=>{
