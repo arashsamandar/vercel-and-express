1c66631df08c5d4dd55fb910d08f422c8e3abb8e
arashsamandar <arash.internet@gmail.com>
2024-08-07 22:15:41 +0330

    new path added to "dist\index.js" too

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -9,3 +9,6 @@ app.listen(3000, () => console.log('application ready to use'));
 app.get('/api/helloarash', (req, res) => {
     res.send('hello arash this one works ?? really ??');
 });
+app.get('/api/helloarash2', (req, res) => {
+    res.send('hello arash this one works On Two 2');
+});
