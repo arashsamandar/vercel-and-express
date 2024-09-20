ffd7b6bd3ce6cb433669a454304994219a83a3e6
arashsamandar <arash.internet@gmail.com>
2024-09-21 00:47:37 +0330

    mongodb atlas again

--- backend/index.ts
+++ backend/index.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // ------------ routes -----------
 app.use('/api/tests/', router);
-app.use('api/database/', mongoRouter);
+app.use('/api/database/', mongoRouter);
 
 app.use((req,res,next)=>{
 	res.status(404).send('404 - not found');
