cec835e826ef0d73289c6828acf0284d987af749
arashsamandar <arash.internet@gmail.com>
2024-09-20 12:20:23 +0330

    compiled js files

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -3,20 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const startTime = Date.now();
 const express_1 = __importDefault(require("express"));
+const testRoutes_1 = __importDefault(require("./src/routes/testRoutes"));
 const app = (0, express_1.default)();
-app.listen(3000, () => console.log('application ready to use'));
-app.get('/api/helloarash', (req, res) => {
-    let responseStartTime = Date.now();
-    res.json({
-        totalTime: Date.now() - startTime,
-        responseTime: Date.now() - responseStartTime,
-    });
-});
-app.get('/api/helloarash2', (req, res) => {
-    res.send('hello arash this one works On Two 2');
-});
+app.use('/api/tests/', testRoutes_1.default);
 app.get('/api/testApplication', (req, res) => {
     res.send('Application Is Working Gracefully Arash');
 });
+app.listen(3000, () => console.log('application ready to use'));
