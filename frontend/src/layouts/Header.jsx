dd6c2019e12d824036463642c818abfebf05121b
arashsamandar <arash.internet@gmail.com>
2024-10-30 17:33:35 +0330

    evening 9 Aban 1403

--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -5,7 +5,7 @@ export default function Header(){
     return (
         <header className="relative bg-gray-700 drop-shadow-lg p-4 text-center min-h-[5vh]">
             <nav className="flex items-center justify-center">
-                <a href="/getbooks" className="absolute left-4 text-2xl font-bold text-white float-left">Laravel Eleven</a>
+                <a href="/" className="absolute left-4 text-2xl font-bold text-white float-left">Laravel Eleven</a>
                 <ul className="flex space-x-4 items-center justify-center">
                     <CustomLink to="/addbooks" name="Add Books" />
                     <CustomLink to="/updatebooks" name="Update Books" />
