ae097382212ae5d45e64279ffba4af0a0974c004
arashsamandar <arash.internet@gmail.com>
2025-05-17 19:19:21 +0330

    holds test only

--- frontend/src/layouts/Footer.jsx
+++ frontend/src/layouts/Footer.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Footer(){
     return(
-        <div className="bg-gray-700 text-center p-4 min-h-[5vh]">
-            <div className="font-bold text-shadow">
-
+        <div className="bg-gray-700 text-center p-4 min-h-[5.43vh]">
+            <div className="font-bold text-shadow text-white">
+                This site is a Playground. a Test only
             </div>
         </div>
     )
