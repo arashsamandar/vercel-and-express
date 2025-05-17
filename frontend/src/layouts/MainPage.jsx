290f784cc95a10ac23842a18f8a9974a9ca215d2
arashsamandar <arash.internet@gmail.com>
2025-05-17 20:09:47 +0330

    cube with no side written

--- frontend/src/layouts/MainPage.jsx
+++ frontend/src/layouts/MainPage.jsx
@@ -32,13 +32,13 @@ export default function MainPage() {
                 <path d="M0 0 C6.27 0 12.54 0 19 0 C19 17.16 19 34.32 19 52 C12.73 52 6.46 52 0 52 C0 34.84 0 17.68 0 0 Z " fill="#url(#movingGradientt)" transform="translate(307,0)" />
             </svg>
             <br/><br/>
-                <div id="cube-container">
-                    <div id="front" className="face">Front</div>
-                    <div id="back" className="face">Back</div>
-                    <div id="right" className="face">Right</div>
-                    <div id="left" className="face">Left</div>
-                    <div id="top" className="face">Top</div>
-                    <div id="bottom" className="face">Bottom</div>
+                <div id="cube-container" style={{marginLeft: "130px"}}>
+                    <div id="front" className="face"></div>
+                    <div id="back" className="face"></div>
+                    <div id="right" className="face"></div>
+                    <div id="left" className="face"></div>
+                    <div id="top" className="face"></div>
+                    <div id="bottom" className="face"></div>
                 </div>
 
         </div>
