61476f1e98e89fe8ad136b9843388c1cb96364f7
arashsamandar <arash.internet@gmail.com>
2025-05-28 21:58:25 +0330

    feat Mesh

--- frontend/src/layouts/Footer.jsx
+++ frontend/src/layouts/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer(){
     return(
-        <div className="bg-gray-700 text-center p-[1.5em]">
+        <div className="bg-gray-700 text-center p-[1.5em] z-20">
             <div className="flex flex-col justify-center text-center text-shadow text-white">
                 <div>
                     <span>Arash's About Me Website ( under construction )</span>
