5e112f385389fed1d4731a322527d0e5c7d43e7a
arashsamandar <arash.internet@gmail.com>
2025-07-19 23:35:57 +0330

    fixing menu for main page

--- frontend/src/components/pages/MainLayout.jsx
+++ frontend/src/components/pages/MainLayout.jsx
@@ -10,10 +10,14 @@ export default function MainLayout() {
         setIsMenuOpen(!isMenuOpen);
     };
 
+    const closeMenu = () => {
+        setIsMenuOpen(false);
+    };
+
     return (
         <div className="flex flex-col min-h-screen">
             <Header isMenuOpen={isMenuOpen} toggleMenu={toggleMenu}/>
-            <main onClick={toggleMenu} className="flex flex-col text-center min-h-[90vh] bg-gradient-to-bl from-sky-500 to-indigo-500">
+            <main onClick={closeMenu} className="flex flex-col text-center min-h-[90vh] bg-gradient-to-bl from-sky-500 to-indigo-500">
                 <Outlet />
             </main>
             <Footer/>
