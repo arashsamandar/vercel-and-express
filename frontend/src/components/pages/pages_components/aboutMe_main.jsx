4602a00b174347a91b9d988e1354bc1444cf2407
arashsamandar <arash.internet@gmail.com>
2025-07-20 19:19:32 +0330

    feat added squires to aboutMe page

--- frontend/src/components/pages/pages_components/aboutMe_main.jsx
+++ frontend/src/components/pages/pages_components/aboutMe_main.jsx
@@ -2,8 +2,8 @@ import * as React from "react";
 
 export default function AboutMe_main() {
     return (
-        <div className="flex flex-col text-center min-h-[90vh] justify-center items-center">
-            <h2 className="font-bold text-xl">
+        <div className="flex flex-col text-center min-h-[90vh] justify-center items-center z-10">
+            <h2 className="font-bold text-xl z-10">
                 i feel information about myself here<br/>
                 also see my Resume
             </h2>
