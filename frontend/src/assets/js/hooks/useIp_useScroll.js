f0fb9b4bf5c1c46d303e4547fa92075cca1d07af
arashsamandar <arash.internet@gmail.com>
2026-05-25 20:33:58 +0330

    (UI) Geographics changed

--- frontend/src/assets/js/hooks/useIp_useScroll.js
+++ frontend/src/assets/js/hooks/useIp_useScroll.js
@@ -7,7 +7,7 @@ export function useIp() {
     useEffect(() => {
         const fetchIp = async () => {
             if(localStorage.getItem("PC_ID") === "771a4fc0-c417-4800-a64d-d0558abf0993") {
-                setIp({message: "Localhost was Detected", latitude: 10, longitude: 10, country_name: "Localhost", city: "Salamander Home", ip: "127.0.0.1"});
+                setIp({message: "Localhost was Detected", latitude: 33, longitude: 54, country_name: "Localhost", city: "Salamander Home", ip: "127.0.0.1"});
                 return;
             }
             const json1 = await fetch("/api/tests/getIp")
@@ -20,7 +20,7 @@ export function useIp() {
                 .then(res => res.json())
                 .catch(err => {
                     console.error("External Geo fetch failed", err);
-                    return { latitude: 51, longitude: 41, country_name: "Error", city: "Proxy" }; // Fallback
+                    return { latitude: 33, longitude: 54, country_name: "Iran", city: "Tehran" }; // Fallback
                 });
             const data = {
                 ip: json1?.ip ?? "0.0.0.0",
