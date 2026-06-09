fc25c86f85376e0ceac9c58899ce4ad622265f01
arashsamandar <arash.internet@gmail.com>
2026-06-09 21:54:46 +0330

    changed phone on card

--- frontend/src/components/pages/pages_components/MainPage/arashCard.jsx
+++ frontend/src/components/pages/pages_components/MainPage/arashCard.jsx
@@ -72,7 +72,7 @@ export default function ArashCard() {
                             <div className="card-contact-wrapper">
                                 <div className="card-contact">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M22 16.92v3a2 2 0 01-2.18 2 19.79 19.79 0 01-8.63-3.07 19.5 19.5 0 01-6-6 19.79 19.79 0 01-3.07-8.67A2 2 0 014.11 2h3a2 2 0 012 1.72 12.84 12.84 0 00.7 2.81 2 2 0 01-.45 2.11L8.09 9.91a16 16 0 006 6l1.27-1.27a2 2 0 012.11-.45 12.84 12.84 0 002.81.7A2 2 0 0122 16.92z"/></svg>
-                                    (0098) 912-736-4767
+                                    (+374) 912-736-4767
                                 </div>
                                 <div className="card-contact">
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"/><path d="M22 6l-10 7L2 6"/></svg>
